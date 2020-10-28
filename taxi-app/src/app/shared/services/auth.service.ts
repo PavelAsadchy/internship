c49ff9bc6c90e76f646776424c7208f3bc96ed12
Pavel Asadchy <Pavel.Asadchy@itechart-group.com>
2020-10-28 17:21:37 +0300

    user model

--- taxi-app/src/app/shared/services/auth.service.ts
+++ taxi-app/src/app/shared/services/auth.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AUTH_URL } from '../consts/consts';
+import { ICredentials } from '../models/credentials.model';
+import { IUser } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +12,19 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  public login(credentials: ICredentials): Observable<any> {
+    return this.http.post(AUTH_URL + 'signin', {
+      username: credentials.username,
+      password: credentials.password
+    })
+  }
+
+  public register(user: IUser): Observable<any> {
+    return this.http.post(AUTH_URL + 'signup', {
+      username: user.username,
+      email: user.email,
+      password: user.password
+    })
+  }
   
 }
--- taxi-app/src/app/shared/services/token.service.ts
+++ taxi-app/src/app/shared/services/token.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class TokenService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public signOut() {}
+
+  public saveToken() {}
+
+  public getToken() {}
+
+  public saveUser() {}
+
+  public getUser() {}
 }
