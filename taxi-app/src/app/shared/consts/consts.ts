b26b8de83614ce8dc9b2bf5a128d4132c2c6db05
Pavel Asadchy <Pavel.Asadchy@itechart-group.com>
2020-10-29 12:29:22 +0300

    feat: implementation of login method in auth service

--- taxi-app/src/app/shared/consts/consts.ts
+++ taxi-app/src/app/shared/consts/consts.ts
@@ -1,1 +1,4 @@
 export const AUTH_URL = '';
+
+export const JWT_TOKEN = 'JWT_TOKEN';
+export const REFRESH_TOKEN = 'REFRESH_TOKEN';
--- taxi-app/src/app/shared/services/auth.service.ts
+++ taxi-app/src/app/shared/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AUTH_URL } from '../consts/consts';
+import { Observable, of } from 'rxjs';
+import { catchError, mapTo, tap } from 'rxjs/operators'
+import { AUTH_URL, JWT_TOKEN, REFRESH_TOKEN } from '../consts/consts';
+import { ITokens } from '../models/tokens.model';
 // import { ICredentials } from '../models/credentials.model';
 import { IUser } from '../models/user.model';
 
@@ -10,14 +12,34 @@ import { IUser } from '../models/user.model';
 })
 export class AuthService {
 
+  public loggedUser: string = null;
+
   constructor(private http: HttpClient) { }
 
   public isLoggedIn(): boolean {
     return false;
   }
 
-  public login(user: IUser): Observable<any> {
-    return this.http.post(AUTH_URL + 'login', user)
+  public login(user: IUser): Observable<boolean> {
+    return this.http.post(AUTH_URL + '/login', user)
+      .pipe(
+        tap((tokens: ITokens) => this.doLoginUser(user.username, tokens)),
+        mapTo(true),
+        catchError(error => {
+          alert(error.error);
+          return of(false);
+        })
+      );
+  }
+
+  private doLoginUser(username: string, tokens: ITokens) {
+    this.loggedUser = username;
+    this.storeTokens(tokens);
+  }
+
+  private storeTokens(tokens: ITokens) {
+    localStorage.setItem(JWT_TOKEN, tokens.jwt);
+    localStorage.setItem(REFRESH_TOKEN, tokens.refreshToken);
   }
 
   public register(user: IUser): Observable<any> {
