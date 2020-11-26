16cad9f472401087ce4d58578664322b639f40f8
Pavel Asadchy <Pavel.Asadchy@itechart-group.com>
2020-11-26 13:06:38 +0300

    refactor: del unused code

--- taxi-app/src/app/pages/booking-list/components/booking-enum/booking-enum.component.ts
+++ taxi-app/src/app/pages/booking-list/components/booking-enum/booking-enum.component.ts
@@ -1,33 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { IBookingOptions } from 'src/app/shared/models/booking-options.model';
-import { BOOKINGS_LOAD_ACTION } from 'src/app/shared/stores/booking-store/booking.actions';
-import {
-  SELECT_BOOKING_LIST,
-  SELECT_BOOKING_LOADING,
-} from 'src/app/shared/stores/booking-store/booking.selector';
-import { IBookingState } from 'src/app/shared/stores/booking-store/booking.state';
-import { DeleteBookingConfirmComponent } from '../delete-booking-confirm/delete-booking-confirm.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-booking-enum',
   templateUrl: './booking-enum.component.html',
   styleUrls: ['./booking-enum.component.scss'],
 })
-export class BookingEnumComponent implements OnInit {
-  bookingList$: Observable<IBookingOptions[]>;
-  isLoading$: Observable<boolean>;
-
-  constructor(private store: Store<IBookingState>, public dialog: MatDialog) {}
-
-  ngOnInit() {
-    this.store.dispatch(BOOKINGS_LOAD_ACTION());
-    this.bookingList$ = this.store.pipe(select(SELECT_BOOKING_LIST));
-    this.isLoading$ = this.store.pipe(select(SELECT_BOOKING_LOADING));
-  }
-  trigger(): void {
-    this.dialog.open(DeleteBookingConfirmComponent);
-  }
-}
+export class BookingEnumComponent {}
