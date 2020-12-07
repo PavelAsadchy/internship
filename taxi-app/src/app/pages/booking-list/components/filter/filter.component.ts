a308a5aa56445f7018b28c0095a1eb9dcd5bafa6
Pavel Asadchy <Pavel.Asadchy@itechart-group.com>
2020-12-07 11:40:18 +0300

    feat: dates filter with params

--- taxi-app/src/app/pages/booking-list/components/filter/filter.component.ts
+++ taxi-app/src/app/pages/booking-list/components/filter/filter.component.ts
@@ -50,23 +50,71 @@ export class FilterComponent {
       statuses: this.filterForm.get('statuses').value,
       dateFrom: moment(this.filterForm.get('dateFrom').value),
       channels: this.filterForm.get('channels').value,
-      dateTo: moment(this.filterForm.get('dateTo').value),
+      dateTo: moment(this.filterForm.get('dateTo').value) || moment(new Date()),
       vehicle: this.filterForm.get('vehicle').value,
     };
-    console.log('smth');
     this.store.dispatch(LOAD_BOOKINGS_BY_FILTER_ACTION({ filterParams }));
   }
 
-  onFilterFormReset() {
+  onDateSetup(dateLag: number, param: string): void {
+    const today = new Date();
+    switch (param) {
+      case 'month':
+        this.filterForm.patchValue({
+          dateFrom: new Date(
+            today.getFullYear(),
+            today.getMonth() - dateLag,
+            today.getDate()
+          ),
+        });
+        break;
+
+      case 'day':
+        this.filterForm.patchValue({
+          dateFrom: new Date(
+            today.getFullYear(),
+            today.getMonth(),
+            today.getDate() - dateLag
+          ),
+        });
+        break;
+
+      case 'hour':
+        this.filterForm.patchValue({
+          dateFrom: new Date(
+            today.getFullYear(),
+            today.getMonth(),
+            today.getDate(),
+            today.getHours() - dateLag
+          ),
+        });
+        break;
+
+      case 'current':
+        this.filterForm.patchValue({
+          dateTo: today,
+        });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  onFilterFormReset(): void {
     this.filterForm.reset();
   }
 
-  onDatesLimitReset() {
+  onDatesLimitReset(): void {
     this.filterForm.get('dateFrom').reset();
     this.filterForm.get('dateTo').reset();
   }
 
-  onLoadAllBookings() {
+  onLoadAllBookings(): void {
     this.store.dispatch(LOAD_BOOKINGS_ACTION());
+  }
+
+  trigger() {
+    console.log(this.filterForm);
   }
 }
