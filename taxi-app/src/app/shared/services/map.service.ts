542090b2802da5df357d14f77f58acfc1b68816f
PavelAsadchy <asadchy.pavel@gmail.com>
2020-12-28 12:07:05 +0300

    refactor

--- taxi-app/src/app/shared/services/map.service.ts
+++ taxi-app/src/app/shared/services/map.service.ts
@@ -1,33 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClientService } from './http-client.service';
-import { GenericService } from './generic.service';
-import { DatePipe } from '@angular/common';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MapService extends HttpClientService {
+export class MapService {
   locationApiUrl: string;
 
-  constructor(
-    http: HttpClient,
-    datePipe: DatePipe,
-    genericService: GenericService
-  ) {
-    super(http, datePipe, genericService);
+  constructor(private http: HttpClient) {
     this.locationApiUrl = environment.locationApiUrl;
   }
 
   getLocation(): Observable<string> {
-    return this.myGet<string>({
-      url: this.locationApiUrl,
-      headers: {
-        name: 'InterceptorSkipHeader',
-        value: '',
-      },
-    });
+    return this.http.get<string>(this.locationApiUrl);
   }
 }
