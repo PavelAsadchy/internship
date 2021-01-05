fe52b88095c0e1d4cfcd1c47d106cd80d067504e
PavelAsadchy <asadchy.pavel@gmail.com>
2021-01-05 11:28:52 +0300

    feat: group model

---
+++ taxi-app/src/apps/admin/src/app/shared/consts/groups.const.ts
@@ -0,0 +1,44 @@
+import { IGroup } from '../models/group.model';
+
+export const DEFAULT_GROUPS: IGroup[] = [
+  {
+    id: '1',
+    name: 'user_basic',
+  },
+  {
+    id: '2',
+    name: 'user_avg',
+  },
+  {
+    id: '3',
+    name: 'user_adv',
+  },
+  {
+    id: '4',
+    name: 'user_prem',
+  },
+  {
+    id: '5',
+    name: 'manager_basic',
+  },
+  {
+    id: '6',
+    name: 'manager_avg',
+  },
+  {
+    id: '7',
+    name: 'manager_adv',
+  },
+  {
+    id: '8',
+    name: 'manager_prem',
+  },
+  {
+    id: '9',
+    name: 'management',
+  },
+  {
+    id: '10',
+    name: 'admin',
+  },
+];
