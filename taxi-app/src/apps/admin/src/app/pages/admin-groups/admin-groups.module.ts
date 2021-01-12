7d7bb39280f40f7b98043ff634f6048639ff5304
PavelAsadchy <asadchy.pavel@gmail.com>
2021-01-12 17:11:56 +0300

    fix: set loaders as shared module

--- taxi-app/src/apps/admin/src/app/pages/admin-groups/admin-groups.module.ts
+++ taxi-app/src/apps/admin/src/app/pages/admin-groups/admin-groups.module.ts
@@ -3,10 +3,15 @@ import { AdminGroupsComponent } from './container/admin-groups.component';
 import { SharedModule } from 'src/libs/@shared/shared.module';
 import { AdminGroupsTableComponent } from './components/admin-groups-table/admin-groups-table.component';
 import { AdminGroupsTableBtnsComponent } from './components/admin-groups-table-btns/admin-groups-table-btns.component';
+import { ProgressModule } from 'src/libs/@modules/progress/progress.module';
 
 @NgModule({
-  declarations: [AdminGroupsComponent, AdminGroupsTableComponent, AdminGroupsTableBtnsComponent],
-  imports: [SharedModule],
+  declarations: [
+    AdminGroupsComponent,
+    AdminGroupsTableComponent,
+    AdminGroupsTableBtnsComponent,
+  ],
+  imports: [SharedModule, ProgressModule],
   exports: [AdminGroupsComponent],
 })
 export class AdminGroupsModule {}
--- taxi-app/src/apps/booking/src/app/pages/booking-edit/booking-edit.module.ts
+++ taxi-app/src/apps/booking/src/app/pages/booking-edit/booking-edit.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BookingModule } from 'src/apps/booking/src/app/shared/modules/booking/booking.module';
 import { MapModule } from 'src/apps/booking/src/app/shared/modules/map/map.module';
+import { ProgressModule } from 'src/libs/@modules/progress/progress.module';
 import { SharedModule } from 'src/libs/@shared/shared.module';
 import { BookingEditComponent } from './container/booking-edit.component';
 
 @NgModule({
   declarations: [BookingEditComponent],
-  imports: [SharedModule, MapModule, BookingModule],
+  imports: [SharedModule, MapModule, BookingModule, ProgressModule],
   exports: [BookingEditComponent],
 })
 export class BookingEditModule {}
